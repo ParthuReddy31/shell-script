4c3ecf12cb933b36029cdeafc9a3d3bb9321bfc0
parthureddy <parthuredy3@gmail.com>
2025-01-16 09:25:14 -0600

    Again_file-2_is_updated_by_parthu

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,7 +1,7 @@
-PERSON1=ramesh
-PERSON2=suresh
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "$PERSON1:: hi $PERSON2"
-echo "$PERSON2:: hello $PERSON1, how are yu doing..? "
-echo "$PERSON1:: hey $PERSON2, I'm doing well, Thank you. how about you buddy...?"
-echo "$PERSON2:: well!!! I'm doing great!. thank you for asking $PERSON1"
+echo "$PERSON1:: Hi $PERSON2"
+echo "$PERSON2:: Hello $PERSON1, How are you doing..? "
+echo "$PERSON1:: Hey $PERSON2, I'm doing well, Thank you. How about you buddy...?"
+echo "$PERSON2:: Well...!!! I'm doing great!. Thank you for asking $PERSON1"

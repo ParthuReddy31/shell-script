bcf48607b5108ff870369a6fe423df0ee71a0fb3
parthureddy <parthuredy3@gmail.com>
2025-01-16 09:17:51 -0600

    file 2 is updated by parthu

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+PERSON1=ramesh
+PERSON2=suresh
+
+echo "$PERSON1:: hi $PERSON2"
+echo "$PERSON2:: hello $PERSON1, how are yu doing..? "
+echo "$PERSON1:: hey $PERSON2, I'm doing well, Thank you. how about you buddy...?"
+echo "$PERSON2:: well!!! I'm doing great!. thank you for asking man $PERSON1"

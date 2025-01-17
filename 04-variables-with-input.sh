f496d5db6a08ef3dc1c93ec06e22040382a8f689
parthureddy <parthuredy3@gmail.com>
2025-01-16 18:35:01 -0600

    updated_by_parthu

--- 04-variables-with-input.sh
+++ 04-variables-with-input.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo " please enter you username ===>: "
+echo " please enter you username ===>: " #here, the text entered will be cosidered as name variable
 read USERNAME
 echo " entered name is ===>" $USERNAME
 
-echo " please enter you password ===>: "
+echo " please enter you password ===>: " #here, the text entered will be cosidered as password variable
+read USERNAME
 read -s PASSWORD
 echo " entered password is ===>" $PASSWORD

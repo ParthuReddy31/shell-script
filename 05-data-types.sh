a6b3be66a7bbda47b08d488a33b93b8fb7b80182
parthureddy <parthuredy3@gmail.com>
2025-01-17 00:45:49 -0600

    updated by parthu

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -8,5 +8,5 @@ SUB=$(($NUMBER1-$NUMBER2))
 MULTI=$(($NUMBER1*$NUMBER2))
 
 echo "Addition of '$NUMBER1' and '$NUMBER2' is: $SUM "
-echo "Substaction of '$NUMBER1' and '$NUMBER2' is: $SUB "
+echo "Subtraction of '$NUMBER1' and '$NUMBER2' is: $SUB "
 echo "Multiplication of '$NUMBER1' and '$NUMBER2' is: $MULTI "

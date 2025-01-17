b385958905d3f65fe599afb7217d9ee658092348
parthureddy <parthuredy3@gmail.com>
2025-01-17 17:43:31 -0600

    updated_by_parthu

--- 09-install.sh
+++ 09-install.sh
@@ -5,7 +5,23 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "Error:: to install any package user must be Root "
+    exit 1 #other than zero 0
 fi
 
 dnf install mysqld -y
-echo "Mysql is installed"
+if [ $? -ne 0 ]
+then
+    echo " installing MySQL... FAILURE"
+    exit 1
+else
+    echo "the installation of MySQL is SUCCESSFUL"
+fi
+
+dnf install git -y
+if [ $? -ne 0 ]
+then
+    echo " installing GIT... FAILURE"
+    exit 1
+else
+    echo "the installation of GIT is SUCCESSFUL"
+fi

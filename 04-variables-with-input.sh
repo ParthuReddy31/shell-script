8458d6e74d4352a235c446c24888cc3164648fbc
parthureddy <parthuredy3@gmail.com>
2025-01-16 18:31:37 -0600

    updated_by_parthu

---
+++ 04-variables-with-input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo " please enter you username ===>: "
+read USERNAME
+echo " entered name is ===>" $USERNAME
+
+echo " please enter you password ===>: "
+read -s PASSWORD
+echo " entered password is ===>" $PASSWORD

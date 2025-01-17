5e4d6c00913f988e339658c634fd027a2dc14453
parthureddy <parthuredy3@gmail.com>
2025-01-17 00:44:39 -0600

    updated by parthu

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -4,5 +4,9 @@ NUMBER1=$1
 NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2)) #if you want print any dynamic values we have to mention $((condition))
+SUB=$(($NUMBER1-$NUMBER2))
+MULTI=$(($NUMBER1*$NUMBER2))
 
 echo "Addition of '$NUMBER1' and '$NUMBER2' is: $SUM "
+echo "Substaction of '$NUMBER1' and '$NUMBER2' is: $SUB "
+echo "Multiplication of '$NUMBER1' and '$NUMBER2' is: $MULTI "

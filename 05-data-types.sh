249e20785730c58a7bee4008187f50fa8985056c
parthureddy <parthuredy3@gmail.com>
2025-01-17 00:41:00 -0600

    updated by parthu

--- 05-data-types.sh
+++ 05-data-types.sh
@@ -5,4 +5,4 @@ NUMBER2=$2
 
 SUM=$(($NUMBER1+$NUMBER2)) #if you want print any dynamic values we have to mention $((condition))
 
-echo "Addition of '$NUMBER1' and '$NUMBER2' is: '$SUM' "
+echo "Addition of '$NUMBER1' and '$NUMBER2' is: $SUM "

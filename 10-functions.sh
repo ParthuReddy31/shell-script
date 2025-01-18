17b7a9adc89e021f5bb9cdf62fafa3db6bd4ba91
parthureddy <parthuredy3@gmail.com>
2025-01-18 14:26:57 -0600

    updated_by_parthureddy

--- 10-functions.sh
+++ 10-functions.sh
@@ -3,7 +3,7 @@
 USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
-    echo -e " \033[0;35m Error:: to install any package user must be Root "
+    echo -e " \033[0;35m Error:: to install any package user must be Root $N "
     exit 1 #other than zero 0
 fi
 
@@ -11,15 +11,15 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 P="\033[0;35m"
-
+N="\e[0m"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo -e "$R  $2 is FAILURE"
+        echo -e "$R  $2 is FAILURE $N"
         exit 1
     else
-        echo -e "$G $2 is  SUCCESSFUL"
+        echo -e "$G $2 is  SUCCESSFUL $N"
     fi
 }
 
@@ -29,7 +29,7 @@ if [ $? -ne 0 ]
     dnf install mysql -y
     VALIDATE $? "Installing MySQL"
 else
-    echo -e "$G MySQL is already installed"
+    echo -e "$G MySQL is already installed  $N"
 fi
 
 
@@ -39,10 +39,10 @@ then
     dnf install git -y
     VALIDATE $? "Installing GIT"
 else
-    echo -e "$G Git is already installed"
+    echo -e "$G Git is already installed $N"
 fi
 
 echo -e "$R red"
-echo -e "$Y red"
+echo  " red"
 echo -e "$G red"
 echo -e "$P red"

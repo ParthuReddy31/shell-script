7c4e77f9d96067aca77423d36aabb79f6f03100f
parthureddy <parthuredy3@gmail.com>
2025-01-19 22:18:22 -0600

    updated_by_parthureddy

---
+++ 13-for-loop.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+P="\033[0;35m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+# LOGS_FOLDER="/var/log/shellscript-logs"
+# LOG_FILE=$(echo $0 | cut -d "." -f1)
+# LOG_TIMESTAMP=$(date +%y-%m-%d)
+# LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$LOG_TIMESTAMP.log"
+
+LOGS_FOLDER="/var/log/shellscript-logs"
+LOG_FILE=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R  $2 is FAILURE $N"
+        exit 1
+    else
+        echo -e "$G $2 is  SUCCESSFUL $N"
+    fi
+}
+
+echo "Script Started Executing at $LOG_TIMESTAMP " &>>LOG_FILE_NAME
+
+if [ $USERID -ne 0 ]
+then
+    echo -e " $P Error:: to install any package user must be Root $N " &>>LOG_FILE_NAME
+    exit 1 #other than zero 0
+fi
+
+for package in $@
+do
+    dnf list installed $package  &>>LOG_FILE_NAME
+    if [ $? -ne 0 ]
+    then
+        dnf install $package -y  &>>LOG_FILE_NAME
+        VALIDATE $? "Installing package"
+    else
+        echo "$package is already installed"
+    fi
+done
+

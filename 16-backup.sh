b6de93dd20b54197365cf5de9f9d559172e69d06
parthureddy <parthuredy3@gmail.com>
2025-01-22 03:26:30 -0600

    updated_by_parthu

--- 16-backup.sh
+++ 16-backup.sh
@@ -42,4 +42,4 @@ fi
 
 echo "Script started executing at: $TIMESTAMP" &>>$LOG_FILE_NAME
 
-FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime $DAYS)
+FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +DAYS)

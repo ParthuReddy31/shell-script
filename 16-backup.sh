68d3399e7581aefea5ccd34509d67aeadc19988e
parthureddy <parthuredy3@gmail.com>
2025-01-22 00:11:53 -0600

    updated_by_parthu

---
+++ 16-backup.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+SOURCE_DIRECTORY=$1
+DESTINATION_DRIRECTORY=$2
+DAYS=${3:-14}  # if user is not providing number of days, we are taking 14 as default
+
+LOGS_FOLDER="/home/ec2-user/shellscript-logs"
+LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
+
+USAGE1(){
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e " $R Entered command is wrong, please follow USAGE command style"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIRECTORY> <DEST_DIRECTORY> <DAYS(Optional)>"
+    exit 1
+}
+
+USAGE2(){
+    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
+    echo -e "$R USAGE:: $N backup <SOURCE_DIRECTORY> <DEST_DIRECTORY> <DAYS(Optional)>"
+    exit 1
+}
+
+mkdir -p /home/ec2-user/shellscript-logs
+
+if [ $# -le 1 ]
+then
+    USAGE1
+elif [ $# -lt 2 ]
+then
+    USAGE2
+if

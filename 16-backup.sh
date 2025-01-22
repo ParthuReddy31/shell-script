92938050ffe44534148b215f943c14717489dcdf
parthureddy <parthuredy3@gmail.com>
2025-01-22 00:26:25 -0600

    updated_by_parthu

--- 16-backup.sh
+++ 16-backup.sh
@@ -14,26 +14,16 @@ LOG_FILE=$(echo $0 | awk -F "/" '{print $NF}' | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
-USAGE1(){
+USAGE(){
     #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
-    echo -e " $R Entered command is wrong, please follow USAGE command style"
+    echo -e "$R Entered command is wrong, please follow USAGE command style"
     echo -e "$R USAGE:: $N backup <SOURCE_DIRECTORY> <DEST_DIRECTORY> <DAYS(Optional)>"
     exit 1
 }
-
-USAGE2(){
-    #echo -e "$R USAGE:: $N sh 18-backup.sh <SOURCE_DIR> <DEST_DIR> <DAYS(Optional)>"
-    echo -e "$R USAGE:: $N backup <SOURCE_DIRECTORY> <DEST_DIRECTORY> <DAYS(Optional)>"
-    exit 1
-}
-
 
 mkdir -p /home/ec2-user/shellscript-logs
 
 if [ $# -le 1 ];
 then
-    USAGE1
-elif [ $# -lt 2 ];
-then
-    USAGE2
+    USAGE
 fi

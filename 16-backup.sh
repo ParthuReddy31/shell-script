d4c92ffd638c149fb0bdf42b4fce1a1043ad953a
parthureddy <parthuredy3@gmail.com>
2025-01-22 01:05:45 -0600

    updated_by_parthu

--- 16-backup.sh
+++ 16-backup.sh
@@ -30,12 +30,12 @@ fi
 
 if [ ! -d "$SOURCE_DIRECTORY" ]
 then
-    echo -e "$R The Provided $SOURCE_DIRECTORY Source Directory DoecNot Exist... Please Check.!$N"
+    echo -e "$R The Provided $SOURCE_DIRECTORY <-- Source Directory DoecNot Exist... Please Check.!$N"
     exit 1
 fi
 
 if [ ! -d "$DESTINATION_DIRECTORY" ]
 then
-    echo -e "$R The Provided $DESTINATION_DIRECTORY Source Directory DoecNot Exist... Please Check.!$N"
+    echo -e "$R The Provided $DESTINATION_DIRECTORY <-- Destination Directory DoecNot Exist... Please Check.!$N"
     exit 1
 fi

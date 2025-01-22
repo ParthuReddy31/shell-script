1a0dd707b27a04587f15a5c89ff2e28d97a77c34
parthureddy <parthuredy3@gmail.com>
2025-01-21 21:16:28 -0600

    updated_by_parthu

--- 14-delete-old-files.sh
+++ 14-delete-old-files.sh
@@ -41,3 +41,8 @@ echo "Script Started Executing at $TIMESTAMP " &>>LOG_FILE_NAME
 
 FILES_TO_DELETE=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 echo "files to be Deleted: $FILES_TO_DELETE\n"
+
+while read -r file
+do
+    echo "Deleting file:-> $file"   
+done <<< $FILES_TO_DELETE
